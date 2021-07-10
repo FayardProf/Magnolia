99e5f88708951b22082c8a6bd853ebdc59113aac
François Fayard <francois.fayard@auxlazaristeslasalle.fr>
2021-07-10 12:10:43 +0200

    Added a release method to ListD

--- container/ListD.h
+++ container/ListD.h
@@ -19,16 +19,22 @@ struct ListD {
 };
 
 struct ListD* newListD();
+struct ListD* initListD(struct ListD* mem);
 struct ListD* copyListD(struct ListD* other);
 
 void prependListD(struct ListD* self, double x);
 
 void deleteListD(struct ListD* self);
+void releaseListD(struct ListD* self);
 
 struct ListD* newListD() {
-    struct ListD* ans = malloc(sizeof(struct ListD));
-    ans->_head = NULL;
-    return ans;
+    struct ListD* mem = malloc(sizeof(struct ListD));
+    return initListD(mem);
+}
+
+struct ListD* initListD(struct ListD* mem) {
+    mem->_head = NULL;
+    return mem;
 }
 
 void prependListD(struct ListD* self, double x) {
@@ -39,13 +45,17 @@ void prependListD(struct ListD* self, double x) {
 }
 
 void deleteListD(struct ListD* self) {
+    releaseListD(self);
+    free(self);
+}
+
+void releaseListD(struct ListD* self) {
     struct NListD* node = self->_head;
     while (node) {
         struct NListD* next_node = node->next;
         free(node);
         node = next_node;
     }
-    free(self);
 }
 
 #endif //MAGNOLIA_LISTD_H
